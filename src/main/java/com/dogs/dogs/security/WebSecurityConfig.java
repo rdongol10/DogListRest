3197a4b4c0d07ad4517b65603f071e86b08dc189
rabin <rawbean08@gmail.com>
2020-09-19 08:56:47 +0545

    added auth Controller

--- src/main/java/com/dogs/dogs/security/WebSecurityConfig.java
+++ src/main/java/com/dogs/dogs/security/WebSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and().csrf().disable().exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
-				.antMatchers("/api/auth/**").permitAll().antMatchers("/api/dogs/**").permitAll().anyRequest()
+				.antMatchers("/api/auth/**").permitAll().anyRequest()
 				.authenticated();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
